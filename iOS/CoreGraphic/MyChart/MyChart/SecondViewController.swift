2ca6845f677fb63503decfa968130cbd19d291a3
1Consumption <hanseop95@gmail.com>
2020-05-03 02:15:00 +0900

    프로젝트 생성

---
+++ iOS/CoreGraphic/MyChart/MyChart/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyChart
+//
+//  Created by 신한섭 on 2020/05/03.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
